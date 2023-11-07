02bb71332ce1f506a48a3e5c73eef531ce331951
Vladislav <vladislav22009@yandex.ru>
2023-11-07 22:37:36 +0300

    create browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 
+
+////router вынести отдельно??
+//// сами роуты тож отдельно 
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div></div>,  ///заменим компонентой реакт пока так
+    children:[
+      {
+        
+      }
+    ]
+  },
+  {
+
+  }
+])
+
 function App() {
   return (
     <div className="App">
-
+      <RouterProvider router={router} />
     </div>
   );
 }
