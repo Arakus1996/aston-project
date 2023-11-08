6fa7ced4c64cdccf1e24346b9f3bd26718af0229
Vladislav <vladislav22009@yandex.ru>
2023-11-08 13:44:02 +0300

    configured eslitn

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react', 'import'],
+  rules: {
+    'no-console': 'warn',
+    'import/prefer-default-export': 'off',
+    'react/jsx-no-useless-fragment': ['error', { allowExpressions: true }],
+    'no-unused-expressions': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        'newlines-between': 'always-and-inside-groups',
+      },
+    ],
+  },
+}
--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,16 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div></div>, ///заменим компонентой реакт пока так
+    element: <div>f</div>, ///заменим компонентой реакт пока так
     children: [{}],
   },
   {},
 ])
 
-function App() {
+export function App() {
   return (
     <div className='App'>
       <RouterProvider router={router} />
     </div>
   )
 }
-
-export default App
