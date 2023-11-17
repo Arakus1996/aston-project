a22d0e5eb51b504398a30f11be6f204e25457805
Vladislav <vladislav22009@yandex.ru>
2023-11-17 13:57:05 +0300

    refactor: removed getMovies from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { useEffect } from 'react'
 
 import { Home } from './pages/Home/Home'
 import { Navbar } from './widgets/Navbar/ui/Navbar'
-import { useAppDispatch } from './store/hooks'
-import { getMovies } from './shared/api/moviesApi'
 import { Login } from './pages/Login/Login'
 import { Registration } from './pages/Registration/Registration'
 
 export function App() {
-  const dispatch = useAppDispatch()
-  useEffect(() => {
-    dispatch(getMovies())
-  }, [])
-
   return (
     <BrowserRouter basename='/aston-project'>
       <Navbar />
--- src/entities/MovieCards/MovieCards.tsx
+++ src/entities/MovieCards/MovieCards.tsx
@@ -1,10 +1,18 @@
-import { useAppSelector } from '../../store/hooks'
+import { useEffect } from 'react'
+
+import { useAppDispatch, useAppSelector } from '../../store/hooks'
+import { getMovies } from '../../shared/api/moviesApi'
 
 import { MovieCard } from './MovieCard/MovieCard'
 
 import style from './MovieCards.module.css'
 
 export const MovieCards = () => {
+  const dispatch = useAppDispatch()
+  useEffect(() => {
+    dispatch(getMovies())
+  }, [])
+
   const movies = useAppSelector(state => state.movieReducer.movies)
   return (
     <div className={style.cardsBlock}>
