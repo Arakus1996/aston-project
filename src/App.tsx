15a24d9b1d0213201953e802d5eee8bcef709066
Vladislav <vladislav22009@yandex.ru>
2023-11-17 18:59:16 +0300

    fix: deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import { Home } from './pages/Home/Home'
+import { Home } from './pages/home/Home'
 import { Navbar } from './widgets/Navbar/ui/Navbar'
 import { Login } from './pages/Login/Login'
 import { Registration } from './pages/Registration/Registration'
